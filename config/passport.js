5c99630355dd920279031f5301a51cfa089c40ca
Ali Waqas <aliwaqas@codeforpakistan.org>
2020-09-27 00:38:08 +0200

    fixing keys

--- config/passport.js
+++ config/passport.js
@@ -7,7 +7,7 @@ const keys = require('./keys')
 
 const opts = {}
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken()
-opts.secretOrKey = keys.sercet
+opts.secretOrKey = keys.secretOrKey
 
 module.exports = (passport) => {
     passport.use(
